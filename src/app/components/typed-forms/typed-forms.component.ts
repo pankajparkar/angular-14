fb90bbcfa02d2bb5e2e404bf064dc82712fcfab6
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-06-19 15:46:22 +0800

    Added typed forms code.

--- src/app/components/typed-forms/typed-forms.component.ts
+++ src/app/components/typed-forms/typed-forms.component.ts
@@ -1,18 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-typed-forms',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './typed-forms.component.html',
   styleUrls: ['./typed-forms.component.scss']
 })
-export class TypedFormsComponent implements OnInit {
+export class TypedFormsComponent {
 
-  constructor() { }
+  userForm = new FormGroup({
+    name: new FormControl<string>(''),
+    age: new FormControl<number | null>(null),
+    address: new FormGroup({
+      city: new FormControl<string>(''),
+      state: new FormControl<string>('0'),
+      postalCode: new FormControl<number | null>(null),
+    }),
+  });
 
-  ngOnInit(): void {
+  submit() {
+    console.log(this.userForm.value);
+    const { address, age, name } = this.userForm.value
+    // if (address !== undefined) {
+    //   const { city, state, postalCode } = address;
+    // }
   }
 
 }
