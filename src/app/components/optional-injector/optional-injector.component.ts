5e793bb5f25efab0c281f32e6d568449ad826a17
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-06-19 22:38:19 +0800

    Added optional injector demo

--- src/app/components/optional-injector/optional-injector.component.ts
+++ src/app/components/optional-injector/optional-injector.component.ts
@@ -1,16 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-optional-injector',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+  ],
   templateUrl: './optional-injector.component.html',
   styleUrls: ['./optional-injector.component.scss']
 })
 export class OptionalInjectorComponent implements OnInit {
+  htmlTemplateRef = `
+    <ng-template #template let-user>
+      <div>
+          My name is {{ user.name }}
+      </div>
+    </ng-template>
+  `;
+  @ViewChild('vc', { read: ViewContainerRef, static: true }) vc!: ViewContainerRef;
+  @ViewChild('template', { read: TemplateRef, static: true }) template!: TemplateRef<any>;
+  user = { name: 'Default Name' };
 
   constructor() { }
+
+  clear() {
+    this.vc.clear();
+  }
+
+  addContentVc() {
+    this.clear();
+    const view = this.vc.createEmbeddedView(this.template, { $implicit: this.user });
+    this.vc.insert(view);
+  }
+
+  addContentTemplate() {
+    this.clear();
+    const view = this.template.createEmbeddedView({ $implicit: { name: 'Pankaj' } });
+    this.vc.insert(view);
+  }
 
   ngOnInit(): void {
   }
