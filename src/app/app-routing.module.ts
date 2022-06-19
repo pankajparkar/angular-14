9151cb3166a7495838dd13e1620345242ba56fce
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-06-19 14:42:32 +0800

    Configured routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuiltInPrimitivesDialogComponent } from './components/built-in-primitives-dialog/built-in-primitives-dialog.component';
+import { BuiltInPrimitivesMenuComponent } from './components/built-in-primitives-menu/built-in-primitives-menu.component';
+import { InjectComponent } from './components/inject/inject.component';
+import { PageTitleStrategyComponent } from './components/page-title-strategy/page-title-strategy.component';
+import { ProtectedVariableComponent } from './components/protected-variable/protected-variable.component';
+import { TypedFormsComponent } from './components/typed-forms/typed-forms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'typed-forms',
+    component: TypedFormsComponent,
+  },
+  {
+    path: 'inject',
+    component: InjectComponent,
+  },
+  {
+    path: 'built-in-primitivess-menu',
+    component: BuiltInPrimitivesMenuComponent,
+  },
+  {
+    path: 'built-in-primitivess-dialog',
+    component: BuiltInPrimitivesDialogComponent,
+  },
+  {
+    path: 'protected-variable',
+    component: ProtectedVariableComponent,
+  },
+  {
+    path: 'page-title-strategy',
+    component: PageTitleStrategyComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
