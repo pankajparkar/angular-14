2ad626baf4cfbd6770383ef8592a6d798c9103cc
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-06-19 22:18:39 +0800

    Extended diagnostics changes.

--- src/app/components/extended-diagnostics/extended-diagnostics.component.ts
+++ src/app/components/extended-diagnostics/extended-diagnostics.component.ts
@@ -18,8 +18,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 })
 export class ExtendedDiagnosticsComponent implements OnInit {
 
-  testVariable = '';
-  test = '';
+  // testVariable = '';
+  // test = '';
 
   html = `
     {
